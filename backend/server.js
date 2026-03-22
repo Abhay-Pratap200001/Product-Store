53b03035b4227b065dd25cd091872a4cd7c7e670
Abhay-Pratap200001 <abhaypratap94310@gmail.com>
2026-03-22 12:00:02 +0530

    Use arcjet function in server.js file to protect our routes

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,7 @@ import dotenv from "dotenv";
 
 import productRoutes from "./routes/product.Routes.js";
 import { sql } from "./config/connect.db.js";
+import { aj } from "./lib/arcjet.js";
 
 dotenv.config();
 
@@ -18,11 +19,42 @@ app.use(cors());
 app.use(helmet());
 app.use(morgan("dev"));
 
+
+//applying arjet rate-limit to all routes
+app.use(async (req, res, next) => {
+  try {
+    const decision = await aj.protect(req, {
+      requested: 1, //specific that each request consume 1 token
+    });
+
+    if (decision.isDenied()) {
+      if (decision.reason.isRateLimit()) {
+        res.status(429).json({ error: "To Many Requests" });
+      } else if (decision.reason.isBot()) {
+        res.status(403).json({ error: "Bot access denied" });
+      } else {
+        res.status(403).json({ error: "Forbidden" });
+      }
+      return;
+    }
+
+    if (decision.results.some((result) => result.reason.isBot() && result.reason.isSpoofed())){
+      res.status(403).json({ error: "Spoffed bot detected" });
+      return
+    }
+    next();
+  } catch (error) {
+    console.log("Arcjet error", error);
+    next(error)
+  }
+});
+
+
 app.use("/api/products", productRoutes);
 
 async function initDB() {
   try {
-   const co = await sql`
+    const co = await sql`
     CREATE TABLE IF NOT EXISTS products(
       id SERIAL PRIMARY KEY,
       name VARCHAR(255) NOT NULL,
